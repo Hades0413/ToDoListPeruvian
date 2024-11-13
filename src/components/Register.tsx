3b8484645debe3f12cbe3fffb6078e59e26e0dec
ClaudiaZsv <claudiaysz15@gmail.com>
2024-11-13 14:26:45 -0500

    V1.2
    V1.2

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,182 +1,128 @@
 import React, { useState } from "react";
+import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import "../css/AuthForm.css";
+import authFormImg from "../assets/img/authFormImg.jpg";
+import FaUser from "../components/icons/FaUser";
+import FaEnvelope from "../components/icons/FaEnvelope";
+import FaLock from "../components/icons/FaLock";
 
 const Register: React.FC = () => {
   const [nombre, setNombre] = useState("");
   const [correoElectronico, setCorreoElectronico] = useState("");
   const [contrasena, setContrasena] = useState("");
-  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+    e.preventDefault();
 
     // Validar campos
     if (!nombre || !correoElectronico || !contrasena) {
-      setError("Todos los campos son obligatorios");
+      Swal.fire({
+        title: "Error",
+        text: "Todos los campos son obligatorios.",
+        icon: "error",
+        background: "#333",
+        color: "#fff",
+      });
       return;
     }
 
     try {
-      // Enviar los datos al backend en el puerto 5000
+      // Realizar la solicitud al backend
       const response = await axios.post("http://localhost:5000/register", {
         nombre_usuario: nombre,
         email_usuario: correoElectronico,
         contrasena_usuario: contrasena,
       });
 
+      // Manejar el éxito del registro
       if (response.status === 201) {
-        setError(""); // Limpiar el error si todo fue bien
-        alert("Usuario registrado con éxito");
-        navigate("/"); // Redirigir al login después del registro exitoso
+        Swal.fire({
+          title: "¡Éxito!",
+          text: "Usuario registrado con éxito.",
+          icon: "success",
+          background: "#333",
+          color: "#fff",
+        }).then(() => {
+          navigate("/"); // Redirigir al usuario
+        });
       }
-    } catch (err) {
-      // Manejo de error si la respuesta del servidor es incorrecta
-      setError("Hubo un error al registrar el usuario. Intenta de nuevo.");
+    } catch (err: any) {
+      // Manejo específico para correo electrónico duplicado
+      if (err.response && err.response.status === 409) { // 409 Conflict
+        Swal.fire({
+          title: "Advertencia",
+          text: "El correo electrónico ya está registrado. Intenta con otro.",
+          icon: "warning",
+          background: "#333",
+          color: "#fff",
+        });
+      } else {
+        Swal.fire({
+          title: "Error",
+          text: "Hubo un error al registrar el usuario. Intenta de nuevo.",
+          icon: "error",
+          background: "#333",
+          color: "#fff",
+        });
+      }
       console.error("Error al registrar usuario:", err);
     }
   };
 
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        background: "linear-gradient(to right, #8b5cf6, #7c3aed)",
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "400px",
-          backgroundColor: "white",
-          borderRadius: "8px",
-          padding: "24px",
-          boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-        }}
-      >
-        <h2
-          style={{
-            fontSize: "24px",
-            fontWeight: "bold",
-            textAlign: "center",
-            marginBottom: "24px",
-          }}
-        >
-          Registrarse
-        </h2>
-
-        {/* Mostrar mensaje de error si existe */}
-        {error && (
-          <div
-            style={{
-              backgroundColor: "#f8d7da",
-              color: "#721c24",
-              padding: "10px",
-              borderRadius: "4px",
-              marginBottom: "16px",
-            }}
-          >
-            {error}
-          </div>
-        )}
-
-        <form style={{ marginBottom: "24px" }} onSubmit={handleSubmit}>
-          <label
-            style={{ display: "block", marginBottom: "6px", color: "#000" }}
-          >
-            Nombre
-          </label>
-          <input
-            type="text"
-            placeholder="Nombre"
-            value={nombre}
-            onChange={(e) => setNombre(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px 12px",
-              marginBottom: "12px",
-              border: "1px solid #d1d5db",
-              borderRadius: "4px",
-              color: "#000",
-            }}
-          />
-
-          <label
-            style={{ display: "block", marginBottom: "6px", color: "#000" }}
-          >
-            Correo Electrónico
-          </label>
-          <input
-            type="email"
-            placeholder="tu@email.com"
-            value={correoElectronico}
-            onChange={(e) => setCorreoElectronico(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px 12px",
-              marginBottom: "12px",
-              border: "1px solid #d1d5db",
-              borderRadius: "4px",
-              color: "#000",
-            }}
-          />
-
-          <label
-            style={{ display: "block", marginBottom: "6px", color: "#000" }}
-          >
-            Contraseña
-          </label>
-          <input
-            type="password"
-            placeholder="Contraseña"
-            value={contrasena}
-            onChange={(e) => setContrasena(e.target.value)}
-            required
-            style={{
-              width: "100%",
-              padding: "8px 12px",
-              marginBottom: "12px",
-              border: "1px solid #d1d5db",
-              borderRadius: "4px",
-              color: "#000",
-            }}
-          />
-
-          <button
-            type="submit"
-            style={{
-              width: "100%",
-              padding: "8px 12px",
-              backgroundColor: "#4f46e5",
-              color: "white",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-            }}
-          >
-            Registrarse
-          </button>
-        </form>
-
-        <button
-          onClick={() => navigate("/")}
-          style={{
-            width: "100%",
-            textAlign: "center",
-            color: "#4f74e5",
-            backgroundColor: "transparent",
-            border: "none",
-            cursor: "pointer",
-          }}
-        >
-          ¿Ya tienes una cuenta? Inicia sesión!
-        </button>
+    <div className="auth-container">
+      <div className="auth-content">
+        <div
+          className="auth-image"
+          style={{ backgroundImage: `url(${authFormImg})` }}
+        ></div>
+        <div className="auth-form">
+          <h2 className="auth-title">Crea una cuenta</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <FaUser className="input-icon" />
+              <input
+                type="text"
+                className="auth-input"
+                placeholder="Nombre"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <FaEnvelope className="input-icon" />
+              <input
+                type="email"
+                className="auth-input"
+                placeholder="Correo Electrónico"
+                value={correoElectronico}
+                onChange={(e) => setCorreoElectronico(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <FaLock className="input-icon" />
+              <input
+                type="password"
+                className="auth-input"
+                placeholder="Contraseña"
+                value={contrasena}
+                onChange={(e) => setContrasena(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="auth-button">
+              Registrarse
+            </button>
+          </form>
+          <a href="/" className="auth-link">
+            ¿Ya tienes una cuenta? Inicia sesión
+          </a>
+        </div>
       </div>
     </div>
   );
